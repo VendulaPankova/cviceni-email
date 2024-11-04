5a07e30e826530203b783ba530d297dc9bad50c3
Vendula Pankova <vendula.pankov82@gmail.com>
2024-11-04 20:31:42 +0100

    přidám své vypracování

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+function goodbye(name)
+{ const elClosing = document.querySelector(".email__closing")
+    elClosing.textContent = `S pozdravem ${name}`
+ }
 
+ goodbye("Pavel Ovesný")
+
+
+function fillSubject(subject)
+ { const elSubject = document.querySelector(".email__subject")
+    elSubject.textContent = subject
+ }
+fillSubject("Žádost o práci")
+
+function fillBody(body)
+ { const elBody = document.querySelector(".email__body")
+    elBody.textContent = body
+ }
+fillBody("Toto je ukázkový text pro tělo e-mailu.")
+
+function goodbye(name) {
+    const closingElement = document.querySelector('.email__closing');
+    closingElement.textContent = `S pozdravem ${name}`;
+  }
+  function fillBody(body, name) {
+    const bodyElement = document.querySelector('.email__body');
+    bodyElement.textContent = body;
+    
+    goodbye(name);
+  }
